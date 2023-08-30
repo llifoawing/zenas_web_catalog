249ab24e47b4fb851a53dcb1d2ed07bcb5196e05
Laura Li Foa Wing <100870296+llifoawing@users.noreply.github.com>
2023-08-30 16:33:02 -0700

    rename secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake_zena"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
